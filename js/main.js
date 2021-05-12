6eef4cf41cf11c9bba38e07f9fa1ac3537f0ce01
lucascm <lucascm.frc@gmail.com>
2021-05-11 23:28:15 -0300

    add CacheControl

--- js/main.js
+++ js/main.js
@@ -1,3 +1,69 @@
+/**
+ * 
+ * @param {*} d1 
+ * @param {*} d2 
+ * 
+ * Ref: https://pt.stackoverflow.com/questions/86762/calcular-diferen%C3%A7a-entre-duas-datas-para-validar-campos-da-data
+ * Exemplo: 
+    var dataInicio = new Date(document.getElementById("datainicio").value);
+    var dataFim = new Date(document.getElementById("datafim").value);
+    var diffMilissegundos = dataFim - dataInicio;
+    var diffSegundos = diffMilissegundos / 1000;
+    var diffMinutos = diffSegundos / 60;
+    var diffHoras = diffMinutos / 60;
+    var diffDias = diffHoras / 24;
+    var diffMeses = diffDias / 30;
+
+    return !(diffMeses > 3);
+ */
+function getDiffMinutes(dtOlder, dtNewer) {
+
+    const diffMilissegundos = dtNewer - dtOlder;
+    const diffSegundos = diffMilissegundos / 1000;
+    return diffSegundos / 60;
+    
+}
+
+class CacheControl {
+
+    static __path = "repositories"
+
+    static baseObj = {
+        time: new Date(),
+        obj: undefined,
+    }
+
+    static saveCache(obj) {
+
+        this.baseObj.time = new Date();
+        this.baseObj.obj = obj;
+
+        localStorage.setItem(this.__path, JSON.stringify(this.baseObj));
+
+    }
+
+    static loadCache() {
+
+        const item = localStorage.getItem(this.__path);
+        if (!item) return undefined;
+        this.baseObj = JSON.parse(item);
+        console.log('onde estamos', this.baseObj);
+        if (this.isTimeExpired(this.baseObj.time)) return undefined;
+        return this.baseObj.obj;
+
+    }
+
+    static isTimeExpired(time) {
+        
+        const maxMinutes = 30;
+        const diff = getDiffMinutes(new Date(time), new Date());
+        console.log('time diff', diff);
+        return diff > maxMinutes;
+
+    }
+
+}
+
 class Index {
 
     constructor() {
@@ -47,6 +113,9 @@ class Index {
     }
 
     async getRepositories() {
+
+        const cache = CacheControl.loadCache();
+        if (cache) return Promise.resolve(cache);
         
         const url = 'https://api.github.com/users/lucascm/repos';
         
@@ -55,6 +124,7 @@ class Index {
             const response = await fetch(url);
             if (!response.ok) throw new Error(`getRepositories error (${ response.status }): ${await response.text()}`);
             const json = await response.json();
+            CacheControl.saveCache(json);
             return Promise.resolve(json);
 
         } catch (error) {
