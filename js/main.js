fbcea18374e8b9d0ae64416153096c376bc44fdd
lucascm <lucascm.frc@gmail.com>
2021-05-11 22:29:33 -0300

    add fetch to github apis

--- js/main.js
+++ js/main.js
@@ -5,24 +5,66 @@ class Index {
         this.divSocial = document.querySelector('#socialButtons');
         this.templateRepo = document.querySelector('#repoCard');
         this.divRepositories = document.querySelector('#repositories');
-        this.addNewRepoCard('ImportVRA', 'Projeto TCC', 'link1, link2');
+        this.getRepositories()
+            .then(json => this.createListRepo(json))
+            .catch(error => console.log(error));
 
     }
 
-    addNewRepoCard(title, desc, links) {
+    createListRepo(json) {
+
+        console.log(json);
+        const divs = json.map( repo => this.createNewRepoCard(
+            repo.name, 
+            repo.description,
+            repo.language,
+            [repo.html_url, repo.homepage]
+        ));
+
+        this.divRepositories.append(...divs);
+
+    }
+
+    createNewRepoCard(title, desc, tags, links) {
 
         const div = this.templateRepo.content.cloneNode(true);
         const cardTitle = div.querySelector('.title');
         const cardBody = div.querySelector('.description');
+        const cardLang = div.querySelector('.language');
         const cardLinks = div.querySelector('.links');
+
+        const linkToGit = cardLinks.querySelector('.git');
+        const linkToHome = cardLinks.querySelector('.homepage');
+        
         cardTitle.textContent = title;
         cardBody.textContent = desc;
-        cardLinks.textContent = links;
-        this.divRepositories.appendChild(div);
+        cardLang.textContent = tags;
+        linkToGit.textContent = links[0];
+        linkToHome.textContent = links[1];
+        
+        return div;
         
     }
 
+    async getRepositories() {
+        
+        const url = 'https://api.github.com/users/lucascm/repos';
+        
+        try {
+            
+            const response = await fetch(url);
+            if (!response.ok) throw new Error(`getRepositories error (${ response.status }): ${await response.text()}`);
+            const json = await response.json();
+            return Promise.resolve(json);
 
+        } catch (error) {
+
+            console.error(error);
+            return Promise.reject(error.message);
+            
+        }
+
+    }
 
 }
 
